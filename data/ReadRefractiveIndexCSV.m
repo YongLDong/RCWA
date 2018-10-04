5a8344e8318da54991ab438e4d9213d756956849
Yongliang Dong <35979383+YongLDong@users.noreply.github.com>
2018-10-04 12:35:11 -0700

    Add files via upload

---
+++ data/ReadRefractiveIndexCSV.m
@@ -0,0 +1,10 @@
+function [n,k,wls]=ReadRefractiveIndexCSV(name,R1,C1,R2,C2)
+%read n,k data from refractiveindex.info
+%format of csv must be like Si.csv
+%[wls,n] delimiter: row(R1:R2),colum(C1:C2)
+A=csvread(name,R1-1,C1-1,[R1-1,C1-1,R2-1,C2-1]);
+n=A(:,1);
+wls=A(:,2);
+B=csvread(name,R1+2,C1-1,[R1+2,C1-1,R2+2,C2-1]);
+k=B(:,2);
+end
